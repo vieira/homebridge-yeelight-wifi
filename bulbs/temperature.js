1250533c856502158cfd60728e0b367b2397b66f
João Vieira <joao@guuru.com>
2023-08-22 17:18:55 +0100

    New: Improve adaptive lighting and temperature
    Some enhancements and bug fixes to adaptive lighting behavior and color
    temperature support:
    - Reduce the number of network requests by avoiding sending color
      temperature adjustments when the current temperature already matches
      the target color temperature (fixes #99).
    - Keep track of adaptive lighting changes while light if off and
      immediately set the right color temperature when it is powered on
      (fixes #97).
    - Compute the correct (or approximate) hue and saturation (color) for a
      given temperature and report it back to homekit/home when the users
      sets or changes the color temperature (fixes #95).

--- bulbs/temperature.js
+++ bulbs/temperature.js
@@ -1,8 +1,11 @@
+const { colorFromTemperature } = require('../utils');
+
 const Temperature = (Device) =>
   class extends Device {
     constructor(props, platform) {
       super(props, platform);
       this.temperature = props.ct;
+      this.controller = {};
 
       const { ColorTemperature } = global.Characteristic;
 
@@ -35,15 +38,7 @@ const Temperature = (Device) =>
         .updateValue(this.temperature);
 
       // Setup the adaptive lighting controller if available
-      if (
-        platform.api.versionGreaterOrEqual &&
-        platform.api.versionGreaterOrEqual('1.3.0-beta.23')
-      ) {
-        this.alController = new platform.api.hap.AdaptiveLightingController(
-          this.service
-        );
-        this.accessory.configureController(this.alController);
-      }
+      this.configureAdaptiveLightingController(platform);
     }
 
     get temperature() {
@@ -55,26 +50,84 @@ const Temperature = (Device) =>
     }
 
     async setTemperature(mired) {
+      // If we are already in color temperature mode (2) and the current
+      // temperature matches the new temperature there is no need to send
+      // another command.
+      if (this.temperature === mired && this.colorMode === 2) return;
+
+      // If we receive a direct command from the user to go to a certain color
+      // temperature, turn on the device and go to that temperature.
+      if (!this.controller.isAdaptiveLightingActive()) {
+        await this.setPower(true);
+      }
+
+      // If the device is powered off there is no point in sending the command
+      // as it will have no effect. Let's just update our internal state so
+      // we can send the command when the device is powered on.
+      if (!this.power) {
+        this._temperature = mired;
+        return;
+      }
+
       const { temperature: transition = 400 } = this.config.transitions || {};
       const kelvin = 10 ** 6 / mired;
       const req = {
         method: 'set_ct_abx',
         params: [kelvin, 'smooth', transition],
       };
+
       return this.sendCmd(req).then(() => {
         this._temperature = mired;
+        // After set_ct_abx runs the bulb will be in temperature color mode (2).
+        this.colorMode = 2;
+
+        // When adaptive lightning is enabled homebridge already takes care
+        // of updating the color in HomeKit to match the current color temperature.
+        //
+        // However when we set the color temperature directly we have to do it
+        // ourselves.
+        if (!this.controller.isAdaptiveLightingActive()) {
+          const { hue, sat } = colorFromTemperature(mired);
+          super.updateStateFromProp('hue', hue);
+          super.updateStateFromProp('sat', sat);
+        }
       });
     }
 
     updateStateFromProp(prop, value) {
-      if (prop === 'ct') {
-        this.temperature = value;
-        this.service
-          .getCharacteristic(global.Characteristic.ColorTemperature)
-          .updateValue(this.temperature);
-        return;
+      switch (prop) {
+        case 'ct':
+          this.temperature = value;
+          this.service
+            .getCharacteristic(global.Characteristic.ColorTemperature)
+            .updateValue(this.temperature);
+          break;
+        case 'color_mode':
+          this.colorMode = value;
+          break;
+        default:
+          super.updateStateFromProp(prop, value);
       }
-      super.updateStateFromProp(prop, value);
+    }
+
+    configureAdaptiveLightingController(platform) {
+      if (!platform.api.versionGreaterOrEqual) return;
+      if (!platform.api.versionGreaterOrEqual('1.3.0-beta.23')) return;
+
+      this.controller = new platform.api.hap.AdaptiveLightingController(
+        this.service
+      );
+      this.accessory.configureController(this.controller);
+
+      this.service
+        .getCharacteristic(global.Characteristic.On)
+        .on('change', ({ newValue, oldValue, reason }) => {
+          if (reason !== 'write') return;
+          if (!this.controller.isAdaptiveLightingActive()) return;
+          if (!oldValue && newValue) {
+            this.setTemperature(this._temperature--);
+          }
+        });
     }
   };
 
